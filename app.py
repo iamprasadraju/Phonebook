ccc082a9683655b9cd763990b999857e4e64657b
iamprasadraju <g.leelasaiprasadraju@gmail.com>
2024-07-15 00:02:53 +0530

    Web application is now connected to database and require libraries are added to requiremennts.txt

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template,request
+from cs50 import SQL
+
+db = SQL("sqlite:///database.db")
 
 app = Flask(__name__)
 
@@ -10,9 +13,13 @@ def index():
 def home():
     return render_template('create.html')
 
-@app.route('/create')
+@app.route('/create',methods=["POST"])
 def create():
-    ...
+    if request.method == "POST":
+        contact_name = request.form.get("contact_name")
+        contact_num = request.form.get("contact_number")
+        db.execute("INSERT INTO prasad(contact_name,contact_number) VALUES(?,?)",contact_name,contact_num)
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
